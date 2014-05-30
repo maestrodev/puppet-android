79757357e073cc1607d1c03f625175280f19a7c7
Paul Carey <paul.carey@ignition.it>
2014-05-30 14:40:03 +0100

    Add packages as described at http://bit.ly/1gI1nPl

--- manifests/sdk.pp
+++ manifests/sdk.pp
@@ -53,4 +53,8 @@ class android::sdk {
       default  => [],
     })
   }
+
+  if $lsbdistrelease == 14.04 {
+    ensure_packages(["libc6-i386", "lib32stdc++6", "lib32gcc1", "lib32ncurses5", "lib32z1"])
+  }
 }
