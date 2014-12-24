df6b53571a6ac32720a32e9d3a9d58b63eabe569
Carlos Sanchez <carlos@apache.org>
2014-12-24 09:58:12 +0100

    Run modulesync

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,15 @@
+# This file is managed centrally by modulesync
+#   https://github.com/maestrodev/puppet-modulesync
+
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
+  c.treat_symbols_as_metadata_keys_with_true_values = true
+
   c.before(:each) do
     Puppet::Util::Log.level = :warning
     Puppet::Util::Log.newdestination(:console)
   end
-  c.treat_symbols_as_metadata_keys_with_true_values = true
 
   c.default_facts = {
     :operatingsystem => 'CentOS',
@@ -23,3 +27,4 @@ end
 shared_examples :compile, :compile => true do
   it { should compile.with_all_deps }
 end
+
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -2,6 +2,21 @@ require 'puppet'
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
 
+# overriding puppet installation for the RedHat family distros due to
+# puppet breakage >= 3.5
+def install_puppet(host)
+  host['platform'] =~ /(fedora|el)-(\d+)/
+  if host['platform'] =~ /(fedora|el)-(\d+)/
+    safeversion = '3.4.2'
+    platform = $1
+    relver = $2
+    on host, "rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-#{platform}-#{relver}.noarch.rpm"
+    on host, "yum install -y puppet-#{safeversion}"
+  else
+    super()
+  end
+end
+
 RSpec.configure do |c|
 
   # Project root
@@ -14,15 +29,25 @@ RSpec.configure do |c|
 
   c.before :suite do
     hosts.each do |host|
-      # Install module and dependencies
-      puppet_module_install(:source => proj_root, :module_name => 'android')
-
-      if fact('osfamily') == 'RedHat'
-        on host, puppet('module', 'install', 'stahnma/epel', '--version=0.1.0'), { :acceptable_exit_codes => [0,1] }
+      unless (ENV['RS_PROVISION'] == 'no' || ENV['BEAKER_provision'] == 'no')
+        begin
+          on host, 'puppet --version'
+        rescue
+          if host.is_pe?
+            install_pe
+          else
+            install_puppet(host)
+          end
+        end
       end
-      on host, puppet('module', 'install', 'puppetlabs/stdlib', '--version=3.2.0'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'puppetlabs/java', '--version=1.2.0'), { :acceptable_exit_codes => [0,1] }
-      on host, puppet('module', 'install', 'maestrodev/wget', '--version=1.0.0'), { :acceptable_exit_codes => [0,1] }
+
+      # Install module and dependencies
+      puppet_module_install(:source => proj_root, :module_name => File.basename(proj_root).gsub(/^puppet-/,''))
+
+      on host, puppet('module', 'install', 'puppetlabs-stdlib', '--version=3.2.0'), { :acceptable_exit_codes => [0,1] } 
+      on host, puppet('module', 'install', 'puppetlabs-java', '--version=1.2.0'), { :acceptable_exit_codes => [0,1] } 
+      on host, puppet('module', 'install', 'puppetlabs-wget', '--version=1.0.0'), { :acceptable_exit_codes => [0,1] } 
+      on host, puppet('module', 'install', 'stahnma-epel', '--version=0.1.0'), { :acceptable_exit_codes => [0,1] } if fact('osfamily') == 'RedHat'
     end
   end
 
