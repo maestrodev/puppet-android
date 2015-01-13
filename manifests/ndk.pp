951b0fd71df43f93f3e011c3095ec81e2aaa72de
Sam Kerr <sam@samuelkerr.com>
2015-01-13 11:44:03 -0500

    Fixing NDK puppet-lint errors and adding a basic test.

--- manifests/ndk.pp
+++ manifests/ndk.pp
@@ -1,25 +1,42 @@
+# == Class: android::ndk
+#
+# This downloads and unpacks the Android NDK from Google's download
+# sever, or another specified server.
+#
+# Note that the NDK is expected to be a self extracting, so the
+# older .tar.gz packages that are simply extracted will not be supported.
+#
+# === Authors
+#
+# Sam Kerr <kerr.sam@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2015 Sam kerr, unless otherwise noted.
+#
 class android::ndk(
   $ndk_version = $android::params::ndk_version
-) inherits android::params {
-
+)
+{
+  include android::params
   include wget
 
   $base_path = "http://dl.google.com/android/ndk/${ndk_version}"
   $ndk_installer = "${android::paths::installdir}/${ndk_version}"
   wget::fetch { 'download-androidndk':
-    source => $base_path,
+    source      => $base_path,
     destination => $ndk_installer
   } ->
   file { 'android-ndkexecutable':
     ensure => present,
-    path => $ndk_installer,
+    path   => $ndk_installer,
     owner  => $android::user,
     group  => $android::group,
-    mode => '0755'
+    mode   => '0755'
   } ->
   exec { 'run-androidndk':
     command => $ndk_installer,
-    cwd => $android::params::installdir,
+    cwd     => $android::params::installdir,
   }
 
 }
---
+++ tests/init.pp
@@ -0,0 +1,3 @@
+class { 'android::ndk':
+  ndk_version => 'android-ndk-r10c-linux-x86_64.bin'
+}
